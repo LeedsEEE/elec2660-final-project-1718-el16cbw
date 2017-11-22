873e46ddadcb5fcbf9913bbc1c77fe2f4c420795
Conor Williams [el16cbw] <el16cbw@eentpc95.leeds.ac.uk>
2017-11-22 17:49:27 +0000

    Added lines to 'trainedOn' string so it fits inside label. Set lines of uiLabel to 0 so it adjusts to size of string

--- Password Screen/StaffInfoDataModel.m
+++ Password Screen/StaffInfoDataModel.m
@@ -18,47 +18,47 @@
         self.staffInformation = [NSMutableArray array];
         StaffInformation *name1 = [[StaffInformation alloc] init];
         name1.name = @"Conor Williams";
-        name1.dateJoined = @"15/02/2014";
-        name1.emergencyContactName = @"Luce Kelly";
-        name1.emergencyContactNumber = @"07584126589";
-        name1.languagesSpoken = @"Enlgish, Welsh";
-        name1.trainedOn = @"Green Dragon, Solar Splash, Moon Karts, Jungle Boats, Slide, Pillow, Archery, Costume";
+        name1.dateJoined = @"Joined on: 15/02/2014";
+        name1.emergencyContactName = @"Emergency contact name: Luce Kelly";
+        name1.emergencyContactNumber = @"Emergency contact number: 07584126589";
+        name1.languagesSpoken = @"Speaks: Enlgish, Welsh";
+        name1.trainedOn = @"Trained on: Green Dragon, Solar Splash,\n Moon Karts, Jungle Boats, Slide, Pillow,\n Archery, Costume";
 
         self.staffInformation = [NSMutableArray array];
         StaffInformation *name2 = [[StaffInformation alloc] init];
         name2.name = @"Declan Williams";
-        name2.dateJoined = @"25/04/2016";
-        name2.emergencyContactName = @"Mark Williams";
-        name2.emergencyContactNumber = @"0779258432";
-        name2.languagesSpoken = @"Enlgish, Welsh";
-        name2.trainedOn = @"Moon Karts, Jungle Boats, Slide, Pillow";
+        name2.dateJoined = @"Joined on: 25/04/2016";
+        name2.emergencyContactName = @"Emergency contact name: Mark Williams";
+        name2.emergencyContactNumber = @"Emergency contact number: 0779258432";
+        name2.languagesSpoken = @"Speaks: Enlgish, Welsh";
+        name2.trainedOn = @"Trained on: Moon Karts, Jungle Boats,\n Slide, Pillow";
     
         self.staffInformation = [NSMutableArray array];
         StaffInformation *name3 = [[StaffInformation alloc] init];
         name3.name = @"Niall Williams";
-        name3.dateJoined = @"27/03/2012";
-        name3.emergencyContactName = @"Mark Williams";
-        name3.emergencyContactNumber = @"07548474595";
-        name3.languagesSpoken = @"Enlgish, Welsh";
-        name3.trainedOn = @"Green Dragon, Solar Splash, Moon Karts, Jungle Boats, Slide, Pillow, Archery";
+        name3.dateJoined = @"Joined on: 27/03/2012";
+        name3.emergencyContactName = @"Emergency contact name: Mark Williams";
+        name3.emergencyContactNumber = @"Emergency contact number: 07548474595";
+        name3.languagesSpoken = @"Speaks: Enlgish, Welsh";
+        name3.trainedOn = @"Trained on: Green Dragon, Solar Splash,\n Moon Karts, Jungle Boats, Slide,\n Pillow, Archery";
         
         self.staffInformation = [NSMutableArray array];
         StaffInformation *name4 = [[StaffInformation alloc] init];
         name4.name = @"Brain Brianson";
-        name4.dateJoined = @"03/02/2015";
-        name4.emergencyContactName = @"Brienne Brianson";
-        name4.emergencyContactNumber = @"07234126589";
-        name4.languagesSpoken = @"Enlgish, Welsh, Polish";
-        name4.trainedOn = @"Green Dragon, Moon Karts, Jungle Boats, Slide, Pillow";
+        name4.dateJoined = @"Joined on: 03/02/2015";
+        name4.emergencyContactName = @"Emergency contact name: Brienne Brianson";
+        name4.emergencyContactNumber = @"Emergency contact number: 07234126589";
+        name4.languagesSpoken = @"Speaks: Enlgish, Welsh, Polish";
+        name4.trainedOn = @"Trained on: Green Dragon, Moon Karts,\n Jungle Boats, Slide, Pillow";
     
         self.staffInformation = [NSMutableArray array];
         StaffInformation *name5 = [[StaffInformation alloc] init];
         name5.name = @"Peter vonTrinklestein";
-        name5.dateJoined = @"23/05/2016";
-        name5.emergencyContactName = @"Stephanie vonTrinklestein";
-        name5.emergencyContactNumber = @"07584175314";
-        name5.languagesSpoken = @"Enlgish, Russian";
-        name5.trainedOn = @"Moon Karts, Jungle Boats, Slide, Pillow, Archery";
+        name5.dateJoined = @"Joined on: 23/05/2016";
+        name5.emergencyContactName = @"Emergency contact name: Anna vonTrinklestein";
+        name5.emergencyContactNumber = @"Emergency contact number: 07584175314";
+        name5.languagesSpoken = @"Speaks: Enlgish, Russian";
+        name5.trainedOn = @"Trained on: Moon Karts, Jungle Boats,\n Slide, Pillow, Archery";
         
         /* Add above info to array */
         [self.staffInformation addObject:name1]; //index 0
